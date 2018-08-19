dfd447bc297a09474a18c2e2a0ea9fe4695f81c2
Your Name <jjay.cayabyab@gmail.com>
2018-08-19 14:10:21 -0600

    checked if uploaded to mLab

---
+++ models/Example.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const exampleSchema = new Schema({
+  example: String
+})
+
+mongoose.model("example", exampleSchema);
---
+++ routes/exampleRoutes.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Example = mongoose.model("example");
+
+module.exports = app => {
+  app.get("/example", (req, res) => {
+    res.send("This example route sends back a string.");
+  });
+
+  app.post("/example/new", (req, res) => {
+    new Example({ example: "Example data" }).save();
+    res.send("Hey!");
+  });
+};
