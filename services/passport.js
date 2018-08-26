7449e39d36864e12b5f62ef925743af54565c171
Your Name <jjay.cayabyab@gmail.com>
2018-08-26 15:39:43 -0600

    fixed proxy

--- services/passport.js
+++ services/passport.js
@@ -19,7 +19,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
       const existingUser = await User.findOne({ userId: profile.id });
