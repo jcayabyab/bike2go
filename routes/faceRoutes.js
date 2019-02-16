fce1475fcc9a5f7dfed2c0528c5de7c941a72e2e
shamin <shamin99@outlook.com>
2019-02-16 16:45:11 -0700

    merge

--- routes/faceRoutes.js
+++ routes/faceRoutes.js
@@ -3,10 +3,33 @@ const User = mongoose.model("users");
 
 module.exports = app => {
   app.get("/api/face/new", (req, res) => {
-    res.send("Hello");
     // base64 string for the image
     // get a user based on ID from front end
-
+    const imageUrl = 'https://scontent-sea1-1.xx.fbcdn.net/v/t1.15752-9/s2048x2048/52638768_561950294307804_3120951123943358464_n.jpg?_nc_cat=103&_nc_ht=scontent-sea1-1.xx&oh=a2855a5d84ca22b767283c9ce3b4a773&oe=5CDAFBBD';
+    const subscriptionKey = 'b9db8d7c659246ab9425e9f84a590021';
+    const user = {firstName: "Bob", lastName: "Smith"};
+    //first to get a person ID
+    const params = {
+      'personGroupId': 'bike2go'
+    }
+    const options = {
+      uri: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/persongroups/bike2go/persons',
+      qs: params,
+      body: '{"name" : ' + user.firstName +'}',
+      headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key' : subscriptionKey
+      }
+    }
+    request.post(options, (error, response, body) => {
+      if (error) {
+        console.log('Error: ', error);
+        return;
+      }
+      let returnVal = JSON.stringify(JSON.parse(body), null, '  ');
+      console.log('PersonID Response\n');
+      console.log(' XXX ' + returnVal)
+    });
     // make a new Person inside of the API server
     // upload the face to the Person -> look at return value?
     // DATABASE **
