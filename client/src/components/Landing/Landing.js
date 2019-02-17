52633f9bd7314dc4aeb034b7226f23bd3abb2bc9
Jofred Cayabyab <jjay.cayabyab@gmail.com>
2019-02-17 11:54:13 -0700

    fix redirect from webcam

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,16 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import img from "./biking.jpg";
+import { Col } from "reactstrap";
 
-const Wrapper = styled.div`
+const Row = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  padding: 50px 240px;
-  flex-direction: column;
   color: white;
-  background-color: #14bd6c;
-`;
+  padding: 10px;
+`
 
 const Img = styled.img`
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
@@ -18,21 +15,23 @@ const Img = styled.img`
 `;
 
 const Landing = () => (
-  <Wrapper>
-    <div style={{ margin: "5px" }}>
+  <Row style={{backgroundColor: "#15ae6d"}}>
+    <Col md={5}>
       <p>
         Bike2Go is a service that lets you ride, wherever, whenever you want.
         Make an account to get access to bike racks all over the world.
       </p>
-    </div>
-    <Img src={img} alt="Two happy minorities ride a bike" />
-    <p>
-      <i>
-        Created by Armaan Mohar, Avneet Gill, Jofred Cayabyab, Kelvin Tran,
-        Shamin Rahman.
-      </i>
-    </p>
-  </Wrapper>
+    </Col>
+    <Col md={7}>
+      <Img src={img} alt="Two happy minorities ride a bike" width="600px" />
+      <p>
+        <i>
+          Created by Armaan Mohar, Avneet Gill, Jofred Cayabyab, Kelvin Tran,
+          Shamin Rahman.
+        </i>
+      </p>
+    </Col>
+  </Row>
 );
 
 export default Landing;
